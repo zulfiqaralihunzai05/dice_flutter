b94eacfb792be892c289c1fc601a1d64f5281933
ZulfiqarAli <zulfi4u95@gmail.com>
2021-03-15 15:31:07 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/northzeedevelopers/dice_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.northzeedevelopers.dice_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Dice"),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(
+        ),
+      ),
+    ),
+  );
+}
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 2;
+  int rightDiceNumber =5;
+
+  void getDice(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+               getDice();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+
+            ),
+          ),
+
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                getDice();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+  }
